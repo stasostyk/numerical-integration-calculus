dc86c0c4b2a2ae9418b59ce8fc569eed991aefd3
stashito <s.ostyks@gmail.com>
2022-01-16 22:49:39 +0300

    update on js

--- ball.js
+++ ball.js
@@ -9,7 +9,10 @@ class Ball {
     this.x = x;
     this.y = y;
 
-    this.g = 0.1;
+    this.g = 981;
+
+    this.w = 500;
+    this.h = 500;
   }
 
   draw() {
@@ -18,39 +21,50 @@ class Ball {
   }
 
   bounce() {
-    if (this.x+this.r/2 >= width || this.x-this.r/2 <= 0)
+    if (this.x+this.r/2 >= this.w || this.x-this.r/2 <= 0)
         this.vx *= -1;
 
-    if (this.y+this.r/2 >= height || this.y-this.r/2 <= 0)
+    if (this.y+this.r/2 >= this.h || this.y-this.r/2 <= 0)
       this.vy *= -1;
   }
 
-  standard() {
-    this.x += this.vx;
-    this.vy += this.g;
-    this.y += this.vy;
+  standard(timestep) {
+    this.x += this.vx*timestep;
+    this.vy += this.g*timestep;
+    this.y += this.vy*timestep;
 
     this.bounce();
   }
 
-  euler(dt) {
-    for (let i = 0; i < dt; i++) {
-      this.x += this.vx/dt;
-      this.vy += this.g/dt;
-      this.y += this.vy/dt;
+  euler(timestep, h) {
+    for (let i = 0; i < 1/h; i++) {
+      this.x += this.vx*timestep*h;
+      this.vy += this.g*timestep*h;
+      this.y += this.vy*timestep*h;
 
     this.bounce();
     }
   }
 
-  new_vel(x, t) {
-    return x + this.g*t;
+  dydt(t) {
+    return this.vy + t*this.g;
   }
 
-  runge_kutta_4(dt) {
-    let k1, k2, k3, k4;
+  runge_kutta_4(timestep) {
+    // constant rates of change
+    this.x += this.vx*timestep;
 
-    k1 = dt*new_vel(this.x, 1)
+    // position
+    let k1, k2, k3, k4;
+    k1 = this.dydt(timestep * 0);
+    k2 = this.dydt(timestep * 0.5);
+    k3 = this.dydt(timestep * 0.5);
+    k4 = this.dydt(timestep * 1);
+
+    this.y += timestep * (k1 + 2*k2 + 2*k3 + k4)/6;
+    this.vy += this.g*timestep;
+
+    this.bounce();
   }
 
 }
--- engine.js
+++ engine.js
@@ -1,10 +1,21 @@
 let balls = [];
 
+// time in seconds since simulation started
+let t = 0;
+
+// time elapsed per frame
+let framerate = 30;
+let timestep = 1/framerate;
 
 function setup() {
-  createCanvas(500, 500);
-  balls.push(new Ball(20, 10, 2, 0, 250, 250, 'green'));
-  balls.push(new Ball(20, 10, 2, 0, 250, 250, 'blue'));
+  createCanvas(1000, 500);
+
+  //            radius, mass, vx, vy, x, y, color
+  balls.push(new Ball(20, 10, 50, 0, 250, 250, 'blue'));
+  balls.push(new Ball(20, 10, 50, 0, 250, 250, 'green'));
+  balls.push(new Ball(20, 10, 50, 0, 250, 250, 'purple'));
+
+  frameRate(framerate);
 }
 
 function draw() {
@@ -19,6 +30,9 @@ function draw() {
 }
 
 function update() {
-  balls[0].euler(1000);
-  balls[1].standard();
+  // every frame, increase the time by this timestep
+  t += timestep;
+  balls[0].standard(timestep);
+  balls[1].euler(timestep, 0.1);
+  balls[2].runge_kutta_4(timestep);
 }
